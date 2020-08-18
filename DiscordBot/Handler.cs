a34525736b278fdf6898415fbdd611736782a662
Muizz Zafar <muizzzafar25351964@gmail.com>
2020-08-18 00:36:28 -0400

    initial commit

---
+++ DiscordBot/Handler.cs
@@ -0,0 +1,27 @@
+﻿using Discord;
+using Discord.WebSocket;
+using System;
+using Discord.Commands;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordBot {
+    
+    public class Handler {
+        private DiscordSocketClient client; 
+
+        private static Task log(LogMessage msg) {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+
+        public async Task init() {
+            client.Log += log;
+        }
+
+        public Handler(DiscordSocketClient client) {
+            this.client = client;
+        }
+    }
+}
---
+++ DiscordBot/Program.cs
@@ -0,0 +1,49 @@
+﻿using Discord;
+using Discord.WebSocket;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace DiscordBot {
+
+    public class Program {
+        private readonly string token;
+
+        private DiscordSocketClient client;
+        private Handler handler;
+
+        private Program(string token) {
+            this.token = token;
+        }
+
+        public async Task start() {
+            client = new DiscordSocketClient();
+            handler = new Handler(client);
+            await handler.init();
+
+            await client.LoginAsync(TokenType.Bot, token);
+            await client.StartAsync();
+
+            // The task will continue forever
+            await Task.Delay(-1);
+        }
+
+        // Entry point of the program
+        static void Main(string[] args) {
+            // Init command with token.
+            if (args.Length >= 2 && args[0] == "init") {
+                File.WriteAllText("token.txt", args[1]);
+                Console.WriteLine("Writing token to 'token.txt'");
+            }
+
+            // Start bot with token from "token.txt" in working folder.
+            try {
+                var token = File.ReadAllText("token.txt").Trim();
+                Console.WriteLine("Read token from 'token.txt' sucessfully");
+                new Program(token).start().GetAwaiter().GetResult();
+            } catch (IOException) {
+                Console.WriteLine("Could not read from token.txt. Did you run `init <token>`?");
+            }
+        }
+    }
+}
