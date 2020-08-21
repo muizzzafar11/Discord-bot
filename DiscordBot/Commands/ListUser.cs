8cb8d854682c0e6fe23f2cf25a01f322cc0c1995
Muizz Zafar <muizzzafar25351964@gmail.com>
2020-08-21 19:48:56 -0400

    fixed message length problem

--- DiscordBot/Commands/ListUser.cs
+++ DiscordBot/Commands/ListUser.cs
@@ -9,10 +9,31 @@ namespace DiscordBot.Commands {
     public class ListUser : ModuleBase<SocketCommandContext> {
         
         private readonly string [,] origNames = {
-            {"John Unknown","Doe"},
             {"Muhammad Muizz","Ali"},
             {"Taylor","Whatley"},
+            {"Dev","Narula"},
+            {"Timur","Khayrullin"},
+            {"Zaynah","nolastname"},
+            {"Yifan","Wang"},
+            {"Tahmeed","Naser"},
+            {"Shrena","Sribalan"},
+            {"Rick","(O20)"},
+            {"Pranav","Vyas"},
+            {"Karen","Truong"},
+            {"Muhammad Muizz","nolastname"},
+            {"Vince","Li"},
+            {"Alex","Li"},
+            {"nofirstname","Nolan"},
+            {"Borna","Sadeghi"},
+            {"Hargun","nolastname"},
+            {"Markos","Georghiades"},
             {"Muhammad Muizz","Zafar"},
+            {"Taylor desgroup","Whatley"},
+            {"Viktor fasest","Korolyuk"},
+            {"Denys","Linkov"},
+            {"Muhammad Ali","Syed"},
+            {"Muhammad Muizz","Zafar"},
+            {"John Unknown","Doe"},
         };
         
         [Command("names")]
@@ -36,10 +57,10 @@ namespace DiscordBot.Commands {
         [Summary("compares names of discord guild members with a given list and outputs the names that match")]
         public async Task matchNames(ulong roleId) {
             var users = Context.Guild.Users;
-
-            for (int i = 0; i < origNames.Length; i++) {
+            string discordMessage = "";
+            for (int i = 0; i < origNames.GetLength(0); i++) {
                 foreach (var user in users) {
-                    
+                        
                     string fullName = user.Nickname;
                     if (fullName == null) {
                         fullName = user.ToString();
@@ -47,41 +68,100 @@ namespace DiscordBot.Commands {
                     }
 
                     int spaceIndex = fullName.LastIndexOf(' ');
-                    
-                    // Normal case of where the person has a normal first and last name
-                    if (spaceIndex >= 2) {
-                        string firstName = fullName.Substring(0, spaceIndex);
-                        string lastName = fullName.Substring(spaceIndex + 1);
-                        // If last name matches then proceed to see if the first name is the same as well.
-                        // If both of them match then give the user the role passed in by the user
-                    
-                        // Checking if the last names match
-                        if (origNames[i, 1].Contains(lastName) || lastName.Contains(origNames[i, 1])) {
-                            // Checking if the first names match
-                            if(origNames[i,0].Contains(firstName.Replace(" ", "")) ||
-                                firstName.Replace(" ", "").Contains(origNames[i,0])) {
-                                // await ReplyAsync($"The first name: `{origNames[i,0]}` matches with `{firstName}`");
-                                // await ReplyAsync($"The last name: `{origNames[i,1]}` matches with `{lastName}`");
-                                // await ReplyAsync($"Full name: `{origNames[i,0]} {origNames[i,1]}` matches with `{firstName} {lastName}`");
-                                
-                                // Assigning user a role specified in the command
-                                var role = Context.Guild.GetRole(roleId);
-                                await (user as IGuildUser).AddRoleAsync(role);
-                                await ReplyAsync($"`{firstName} {lastName}` has been granted role `{role}`");
-                                
-                                break;
-                            }
+
+                    Console.WriteLine("index: " + i);
+                    if (spaceIndex <= 3) {
+                        if (compare((origNames[i, 0] + origNames[i, 1]),fullName)) {
+                            discordMessage = discordMessage + $" User `{origNames[i, 0]} {origNames[i, 1]}` has either " +
+                                             $"first or last name which matches with `{fullName}`\n";
+                            // await ReplyAsync($" User `{origNames[i, 0]} {origNames[i, 1]}` has either " +
+                                // $"first or last name which matches with `{fullName}`");
+                            Console.WriteLine($" User `{origNames[i, 0]} {origNames[i, 1]}` has either " +
+                                $"first or last name which matches with `{fullName}`");
                         }
                     }
-                    // If the user has only one name in discord
-                    else {
-                        if ((origNames[i, 0] + origNames[i, 1]).Contains(fullName)) {
-                            // Console.WriteLine($" name {origNames[i, 0] +" "+ origNames[i, 1]} matches with {fullName}");
-                            await ReplyAsync($" name {origNames[i, 0] +" "+ origNames[i, 1]} matches with {fullName}");
+
+                    /**
+                     * 
+                     * If the person has both first and last names in the server
+                     * If last name matches then proceed to see if the first name matches
+                     * If both of them match then give the user the role passed in the command
+                     * If first name doesn't match then notify in the console that the last name 
+                     * match was sucessful
+                     */
+
+                    else
+                    {
+                        string firstName = fullName.Substring(0, spaceIndex);
+                        string lastName = fullName.Substring(spaceIndex + 1);
+
+                        // Checking if the last names match
+                        if (compare(origNames[i, 1], lastName) ||
+                            compare(lastName, origNames[i, 1]))
+                        {
+                            // Checking if the first names match
+                            if (compare(origNames[i, 0], firstName) ||
+                                compare(firstName, origNames[i, 0]))
+                            {
+
+                                // Assigning user a role specified in the command
+                                var role = Context.Guild.GetRole(roleId);
+                                // await (user as IGuildUser).AddRoleAsync(role);
+                                // await ReplyAsync($"`{firstName} {lastName}` has been granted role `{role}`");
+                                discordMessage = discordMessage +
+                                                 $"`{firstName} {lastName}` has been granted role `{role}`\n";
+                                break;
+                            }
+
+                            // If first name doesn't match
+                            // await ReplyAsync($"`{lastName}` of `{firstName} {lastName}` matches " +
+                            // $"with `{origNames[i, 1]}` of `{origNames[i, 0]} {origNames[i, 1]}`");
+                            discordMessage = discordMessage + $"`{lastName}` of `{firstName} {lastName}` matches " +
+                                             $"with `{origNames[i, 1]}` of `{origNames[i, 0]} {origNames[i, 1]}`\n";
+                            Console.WriteLine($"`{lastName}` of `{firstName} {lastName}` matches " +
+                                              $"with `{origNames[i, 1]}` of `{origNames[i, 0]} {origNames[i, 1]}`");
+                        }
+                        // If last name doesn't match
+                        else {
+                            discordMessage = discordMessage + $"{firstName} {lastName} could not be found in the server\n";
                         }
                     }
                 }   
             }
+
+            // await ReplyAsync(discordMessage);
+            await sendSplitMessage(discordMessage);
+            // 0 - 12, 13 -  36, 37 - 52
+            // await sendSplitMessage("0123456789 10\n0123456789 11\n0123456789 12\n0123456789 13\n0123456789 14\n0123456789 15");
+        }
+
+        private bool compare(string name, string matchName) {
+            return name.Replace(" ", "").Contains(matchName.Replace(" ", ""));
+        }
+
+        private async Task sendSplitMessage(string mainMessage)
+        {
+            // The most messgaes we can send in the discord chat
+            int messageLimit = 2000;
+            int startIndex = 0;
+            if (mainMessage == null) {
+                return;
+            }
+            if (mainMessage.Length <= messageLimit) {
+                await ReplyAsync(mainMessage);
+                return;
+            }
+            do {
+                // Get the substring of the specified length
+                string subString = mainMessage.Substring(startIndex, messageLimit+1);
+                // Get the last index of new line in the substring of specified length
+                int endIndex = subString.LastIndexOf("\n");
+                // Send the message from the start index to last index of new line character
+                await ReplyAsync(mainMessage.Substring(startIndex, endIndex));
+                // set the start index to end index, +1 to skip over \n character
+                startIndex += endIndex+1;
+            } while (startIndex + messageLimit <= mainMessage.Length);
+            await ReplyAsync(mainMessage.Substring(startIndex));
         }
 
         [Command("give-roles")]
--- DiscordBot/Handler.cs
+++ DiscordBot/Handler.cs
@@ -7,10 +7,11 @@ using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace DiscordBot {
-    
+
     public class Handler {
         private readonly char prefix = '-';
-        
+        private readonly bool showStackTrace = true;
+
         private readonly DiscordSocketClient client;
         private readonly CommandService commands;
         private readonly ServiceProvider services;
@@ -23,44 +24,56 @@ namespace DiscordBot {
         private async Task messageReceived(SocketMessage msg) {
             // Don't process the command if it was a system message
             var message = msg as SocketUserMessage;
-            if (message == null) return;
+            if (message == null)
+                return;
 
             // Create a number to track where the prefix ends and the command begins
             int argPos = 0;
 
             // Determine if the message is a command based on the prefix and make sure no bots trigger commands
-            if (!(message.HasCharPrefix(prefix, ref argPos) || 
+            if (!(message.HasCharPrefix(prefix, ref argPos) ||
                   message.HasMentionPrefix(client.CurrentUser, ref argPos)) ||
                 message.Author.IsBot)
                 return;
 
             // Create a WebSocket-based command context based on the message
             var context = new SocketCommandContext(client, message);
-
             // Execute the command with the command context we just
             // created, along with the service provider for precondition checks.
-            await commands.ExecuteAsync(
-                context: context, 
+            var result = await commands.ExecuteAsync(
+                context: context,
                 argPos: argPos,
                 services: services);
+
+            // Handle any errors
+            if (!result.IsSuccess && result.Error != CommandError.UnknownCommand) {
+                if (showStackTrace && result.Error == CommandError.Exception
+                        && result is ExecuteResult execution) {
+                    await message.Channel.SendMessageAsync(
+                        $"```\n{execution.Exception.Message}\n\n{execution.Exception.StackTrace}\n```");
+                } else {
+                    await message.Channel.SendMessageAsync(
+                        "Halt We've hit an error.\n```\n{result.ErrorReason}\n```");
+                }
+            }
         }
 
         public async Task init() {
             client.Log += log;
             client.MessageReceived += messageReceived;
-            
+
             await commands.AddModulesAsync(Assembly.GetEntryAssembly(), services);
         }
 
         public Handler(DiscordSocketClient client) {
             this.client = client;
-            
+
             commands = new CommandService();
             services = new ServiceCollection()
                 .AddSingleton(client)
                 .AddSingleton(commands)
                 .BuildServiceProvider();
         }
-        
+
     }
 }
